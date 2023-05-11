d56122014678ff52e273c338442f0d2856c44893
jerok <jerok.kim@gmail.com>
2023-05-11 19:58:08 +0900

    init: initial environment setting

---
+++ src/main/java/kim/jerok/practice_spring_25/PracticeSpring25Application.java
@@ -0,0 +1,13 @@
+package kim.jerok.practice_spring_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpring25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpring25Application.class, args);
+    }
+
+}
---
+++ src/test/java/kim/jerok/practice_spring_25/PracticeSpring25ApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.jerok.practice_spring_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpring25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
