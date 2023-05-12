974ace73ea70559246d8174b12aced37a2b29010
jerok <jerok.kim@gmail.com>
2023-05-12 16:09:48 +0900

    feat: view setting, create member done

---
+++ src/main/java/kim/jerok/practice_spring_25/controller/HomeController.java
@@ -0,0 +1,17 @@
+package kim.jerok.practice_spring_25.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+    
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+    
+}
