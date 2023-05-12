68b893acac0db012ab41aa0ef24b462b0bc4951b
jerok <jerok.kim@gmail.com>
2023-05-12 16:29:09 +0900

    feat: item create done

---
+++ src/main/java/kim/jerok/practice_spring_25/controller/BookForm.java
@@ -0,0 +1,19 @@
+package kim.jerok.practice_spring_25.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookForm {
+    
+    private Long id;
+    
+    private String name;
+    private int price;
+    private int stockQuantity;
+    
+    private String author;
+    private String isbn;
+    
+}
