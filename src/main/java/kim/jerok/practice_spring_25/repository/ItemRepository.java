a7210b25d4dcc98a8df017d58e9c3ea5f9e0f2cc
jerok <jerok.kim@gmail.com>
2023-05-12 17:47:40 +0900

    feat: update item done

--- src/main/java/kim/jerok/practice_spring_25/repository/ItemRepository.java
+++ src/main/java/kim/jerok/practice_spring_25/repository/ItemRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class ItemRepository {
-    
+
     private final EntityManager em;
-    
+
     public void save(Item item) {
         if (item.getId() == null) {
             em.persist(item);
@@ -20,14 +20,14 @@ public class ItemRepository {
             em.merge(item);
         }
     }
-    
+
     public Item findOne(Long id) {
         return em.find(Item.class, id);
     }
-    
+
     public List<Item> findAll() {
-        return em.createQuery("select i from Item i, Item.class")
+        return em.createQuery("select i from Item i", Item.class)
                 .getResultList();
     }
-    
+
 }
--- src/main/java/kim/jerok/practice_spring_25/service/ItemService.java
+++ src/main/java/kim/jerok/practice_spring_25/service/ItemService.java
@@ -1,5 +1,6 @@
 package kim.jerok.practice_spring_25.service;
 
+import kim.jerok.practice_spring_25.domain.item.Book;
 import kim.jerok.practice_spring_25.domain.item.Item;
 import kim.jerok.practice_spring_25.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,14 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+    
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
