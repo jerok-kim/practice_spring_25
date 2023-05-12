21fb620b91153f17478ca9df08211660abf5c111
jerok <jerok.kim@gmail.com>
2023-05-12 12:17:20 +0900

    feat: item domain done

---
+++ src/main/java/kim/jerok/practice_spring_25/service/ItemService.java
@@ -0,0 +1,31 @@
+package kim.jerok.practice_spring_25.service;
+
+import kim.jerok.practice_spring_25.domain.item.Item;
+import kim.jerok.practice_spring_25.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+}
